78af63a94be4d88da46d2897b53c73352229846a
allwynwesly <63363489+allwynwesly@users.noreply.github.com>
2020-04-11 00:08:34 +0530

    Hello / Hi Code by WES

---
+++ hello.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+void add(int a) ;
+void add(int a, int b);
+int main() {
+  int x=1;
+  add(4,0);
+  return 0;
+}
+  void add (int a) {
+  std::cout << "hello" << '\n';
+  }
+  void add(int a, int b) {
+    std::cout << "hi";
+  }
