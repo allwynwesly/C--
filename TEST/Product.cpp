880572cac39e60bf71978d6d2aa0031c7ccb06e8
allwynwesly <63363489+allwynwesly@users.noreply.github.com>
2020-04-20 14:23:07 +0530

    Uploaded by WES

---
+++ TEST/Product.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main (){
+float a, b;
+cout<<"Enter two number: ";
+cin >> a;
+cin >> b;
+float product=a*b;
+cout<<"Product = "<<product<<endl;
+return 0;
+}
