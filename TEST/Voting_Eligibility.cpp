55bceb00c240d3db2fb4d02d5b23fa3b8dabaa9c
allwynwesly <63363489+allwynwesly@users.noreply.github.com>
2020-04-30 21:16:04 +0530

    Code Updated by WES

---
+++ TEST/Voting_Eligibility.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main ()
+{
+  int age;
+  cout<<"Enter your age :";
+  cin>>age;
+    if(age>=18)
+    {
+      cout<<"Your are eligible for voting"<<endl;
+    }
+    else
+    {
+      cout<<"Your are not eligible for voting"<<endl;
+    }
+  return 0;
+}
