3f9f9a8821f035377355d85b68d8aaa7315d3a9a
allwynwesly <63363489+allwynwesly@users.noreply.github.com>
2020-04-30 21:50:38 +0530

    Optimised by WES

--- TEST/Voting_Eligibility.cpp
+++ TEST/Voting_Eligibility.cpp
@@ -2,16 +2,20 @@
 using namespace std;
 int main ()
 {
+  char name[30]={0};
   int age;
-  cout<<"Enter your age :";
+  cout<<"\n Please enter your first name : ";//only first name
+  cin>>name;
+  cout<<"\n Please enter your age : ";
   cin>>age;
+  cout<<endl;// this line of code is added to create one extra line of space
     if(age>=18)
     {
-      cout<<"Your are eligible for voting"<<endl;
+      cout<<name<<" is eligible for voting"<<endl;
     }
     else
     {
-      cout<<"Your are not eligible for voting"<<endl;
+      cout<<name<<" is not eligible for voting"<<endl;
     }
   return 0;
 }
